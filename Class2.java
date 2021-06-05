23af23181ab4b462dde7b06eca56832bece00b31
Seedwagon <shomen666@yandex.ru>
2021-06-05 16:14:16 +1000

    test commit

---
+++ Class2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gittestprj;
+
+/**
+ *
+ * @author leha-
+ */
+public class Class2 {
+    private int zhopa;
+    
+    public void set_Zhopa(int value){
+        this.zhopa = value;
+    }
+    public int get_Zhopa(){
+        return this.zhopa;
+    }
+    
+    public void ZhopaInc(int incr){
+        zhopa+=incr;
+    }
+    
+}
